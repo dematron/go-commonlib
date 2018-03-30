f290ccca3e89796560f1a1ca91ff23f22462e52e
Ilya Luschik <_root_@dematron.ru>
2018-03-30 22:38:39 +0300

    Add CheckError and safely goroutines handling Update Bytes2Human, add flexibility

---
+++ error.go
@@ -0,0 +1,41 @@
+package commonlib
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"time"
+
+	"github.com/go-errors/errors"
+)
+
+// CheckError handle incoming errors and support additional options
+// such as trace and logging trace to file
+func CheckError(inerr error, errorTrace bool, errorTracepath string) error {
+	if inerr != nil {
+		log.Printf("Error: %v%s", inerr, Carriage)
+		// trace
+		pc := make([]uintptr, 15)
+		n := runtime.Callers(2, pc)
+		frames := runtime.CallersFrames(pc[:n])
+		frame, _ := frames.Next()
+		log.Printf("%s,:%d %s%s", frame.File, frame.Line, frame.Function, Carriage)
+		if errorTrace {
+			traceFile, err := os.OpenFile(errorTracepath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer traceFile.Close()
+			t := time.Now().Format("2006/01/02 15:04:05 MST")
+			e := errors.New(inerr)
+			traceFile.WriteString(t + Carriage)
+			traceFile.WriteString(e.TypeName() + Carriage)
+			traceFile.Write(e.Stack())
+			// Issue a Sync to flush writes to stable storage.
+			traceFile.Sync()
+		}
+		//
+	}
+	return inerr
+}
--- humanize/number.go
+++ humanize/number.go
@@ -3,18 +3,18 @@ package humanize
 import "fmt"
 
 // Bytes2Human convert uint64 to string like 10.00 KB (if uint64 is 10000 and postfix is "B")
-// Converting proceed in SI system
+// Converting proceed in SI or non-SI system depends on
 // For extended level or if we simply need "Z" and "Y" use - https://github.com/dustin/go-humanize
-func Bytes2Human(n uint64, postfix string) string {
+func Bytes2Human(n uint64, postfix string, divider float64) string {
 	num := float64(n)
 	prefix := []string{"", "K", "M", "G", "T", "P", "E"}
 	unit := ""
 	for _, unt := range prefix {
-		if num < 1000 {
+		if num < divider {
 			unit = unt
 			break
 		}
-		num = num / 1000
+		num = num / divider
 	}
 	return fmt.Sprintf("%3.2f %s%s", num, unit, postfix)
 }
